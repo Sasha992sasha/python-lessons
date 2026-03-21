37afe2f076154b49b4034bb204861f4bd803b3d3
Sasha992sasha <svidkosasa94@gmail.com>
2026-03-21 14:37:49 +0200

    3.py готовий

---
+++ Dz/dz8/3.py
@@ -0,0 +1,12 @@
+
+
+user = "S9s_"
+passwor = "pas"
+
+def login(username , password):
+    assert username == user , "Не правильно" 
+    assert password == passwor , "Не правильно"
+    print('Вхід виконано успішно')
+    
+    
+login('S9s_' , "pas")
