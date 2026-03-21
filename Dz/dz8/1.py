4b6b5ddeb9054ad4ec6f31a4a1162887cc07b2e7
Sasha992sasha <svidkosasa94@gmail.com>
2026-03-21 14:25:10 +0200

    1.py готовий

---
+++ Dz/dz8/1.py
@@ -0,0 +1,5 @@
+import logging
+
+logging.basicConfig(level=logging.INFO , format="%(asctime)s - %(message)s")
+
+logging.info(f'Шось там')
---
+++ conspect/urok8/1.py
@@ -0,0 +1,27 @@
+import logging
+
+# logging.basicConfig(level=logging.DEBUG)
+# logging.debug("debug")
+# logging.info("info")
+# logging.warning("warning")
+# logging.error("error")
+# logging.critical("critical")
+
+# logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w")
+# logging.debug("debug")
+# logging.info("info")
+
+# logging.basicConfig(level=logging.DEBUG,
+#                     filename="logs.log",
+#                     filemode="w",
+#                     format="We have next logging message:%(asctime)s:%(levelname)s - %(message)s")
+#
+# logging.debug("debug")
+# logging.info("info")
+
+# if 2 + 2 == 4:
+#     print("All okey")
+
+# assert 2 + 2 == 5
+
+# assert 2 + 2 == 5, "wrong assert"
