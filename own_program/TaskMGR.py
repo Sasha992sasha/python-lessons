80fef607cb6bbf5b56633df2d7d8ac34e32d70b9
Sasha992sasha <svidkosasa94@gmail.com>
2026-03-25 19:38:40 +0200

    Додав коментарі

--- own_program/TaskMGR.py
+++ own_program/TaskMGR.py
@@ -1,62 +1,62 @@
 import colorama , datetime , re
 
-class Task:
+class Task: #це клас для завдань
     def __init__(self,name,dedline,deskr):
         self.name = name
         self.dedline = dedline
         self.deskr = deskr
 
-class TaskMGR:
+class TaskMGR: #це основа робота планувальника
     def __init__(self):
         self.tasks = []
         try:
-            with open("task.txt", "r", encoding="utf-8") as f:
+            with open("task.txt", "r", encoding="utf-8") as f: #це відкриття файлу
                 for i in f:
                     i = i.strip()
                     name,dedline,deskr = i.strip().split(' | ')
                     self.tasks.append(Task(name,dedline,deskr))
         except :
             print("Шось не те з файлом")
-            a = open("task.txt" , "w" , encoding="utf-8")
+            a = open("task.txt" , "w" , encoding="utf-8") # це створення + заміна
     
-    def save_file(self):
+    def save_file(self): # це зберігання файлу
         with open("task.txt", "w", encoding="utf-8") as f:
             for i in self.tasks:
-                f.write(f"{i.name} | {i.dedline} | {i.deskr} \n")
+                f.write(f"{i.name} | {i.dedline} | {i.deskr} \n") 
     
-    def add(self,file):
+    def add(self,file): # це додавання завдання в список з якого воно попадає в файл
         self.tasks.append(file)
 
-    def remov(self,index):
+    def remov(self,index): #це видалення з списку
         try:
             self.tasks.pop(index)
         except IndexError:
             input('Не той індекс')
 
-    def print_task(self):
+    def print_task(self): #це вивід
         today = datetime.date.today()
         for index , i in enumerate(self.tasks):  
             try:
                 normalDedline = datetime.datetime.strptime(f"{i.dedline}.{today.year}", "%d.%m.%Y").date()
-                t = (normalDedline - today).days
+                t = (normalDedline - today).days #це 'Дельта дат'
                 if t <= 0:
-                    color = colorama.Fore.RED
+                    color = colorama.Fore.RED # це кольори
                 elif t <= 3:
                     color = colorama.Fore.YELLOW
                 else:
                     color = colorama.Fore.GREEN
             except:
                 color = ""
-            print(f"{color}{index}: {i.name} | {i.dedline} | {i.deskr}{colorama.Style.RESET_ALL}")
+            print(f"{color}{index}: {i.name} | {i.dedline} | {i.deskr}{colorama.Style.RESET_ALL}") #це сам вивід з форматуванням
             
-def save_prin():
+def save_prin(): #це вдобна команда щоби не повторюватись по 100 раз
     MGR.save_file()
     MGR.print_task()
     input()
 
 MGR = TaskMGR()
 
-MGR.print_task()
+MGR.print_task() #це вивід
 
 a = input('1-додати 2-прибрать ')
 
@@ -73,16 +73,16 @@ elif a == "2":
     MGR.remov(f)
     save_prin()
 
-elif a not in ("1", "2"):
-    mat = re.match(r"([^\+\d]+)\+(\d+)(.*)", a)
-    b = mat.group(1).strip()
+elif a not in ("1", "2"): #це швидкий набір по типу Name+1description
+    mat = re.match(r"([^\+\d]+)\+(\d+)(.*)", a) #це розподіл
+    b = mat.group(1).strip() #назва
     try:
-        h = int(mat.group(2))
+        h = int(mat.group(2)) #дедлайн
     except:
-        print('Шось не так')
+        print('Шось не так')# зроблено погано але ним ніхто не користується тому всеодно
     d = mat.group(3) if mat.group(3) else "-"
-    c = datetime.date.today() + datetime.timedelta(days=h)
-    c = c.strftime("%d.%m")
+    c = datetime.date.today() + datetime.timedelta(days=h) 
+    c = c.strftime("%d.%m") # це форматування
     e = Task(b,c,d)
     MGR.add(e)
     save_prin()
