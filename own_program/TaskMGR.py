5525bdf627367299f61ef01658fe6345bff0c29a
Sasha992sasha <svidkosasa94@gmail.com>
2026-03-26 18:02:59 +0200

    Змінив назву деяких зміних на приємніші і додав нову функцію "Швидка дата"

--- own_program/TaskMGR.py
+++ own_program/TaskMGR.py
@@ -1,10 +1,12 @@
 import colorama , datetime , re
 
+colorama.init()
+
 class Task: #це клас для завдань
-    def __init__(self,name,dedline,deskr):
+    def __init__(self,name,dl,dsk):
         self.name = name
-        self.dedline = dedline
-        self.deskr = deskr
+        self.dl = dl
+        self.dsk = dsk
 
 class TaskMGR: #це основа робота планувальника
     def __init__(self):
@@ -13,16 +15,17 @@ class TaskMGR: #це основа робота планувальника
             with open("task.txt", "r", encoding="utf-8") as f: #це відкриття файлу
                 for i in f:
                     i = i.strip()
-                    name,dedline,deskr = i.strip().split(' | ')
-                    self.tasks.append(Task(name,dedline,deskr))
+                    name,dl,dsk = i.strip().split(' | ')
+                    self.tasks.append(Task(name,dl,dsk))
         except :
             print("Шось не те з файлом")
-            a = open("task.txt" , "w" , encoding="utf-8") # це створення + заміна
+            with open("task.txt" , "w" , encoding="utf-8"): # це створення + заміна
+                pass
     
     def save_file(self): # це зберігання файлу
         with open("task.txt", "w", encoding="utf-8") as f:
             for i in self.tasks:
-                f.write(f"{i.name} | {i.dedline} | {i.deskr} \n") 
+                f.write(f"{i.name} | {i.dl} | {i.dsk} \n") 
     
     def add(self,file): # це додавання завдання в список з якого воно попадає в файл
         self.tasks.append(file)
@@ -37,8 +40,8 @@ class TaskMGR: #це основа робота планувальника
         today = datetime.date.today()
         for index , i in enumerate(self.tasks):  
             try:
-                normalDedline = datetime.datetime.strptime(f"{i.dedline}.{today.year}", "%d.%m.%Y").date()
-                t = (normalDedline - today).days #це 'Дельта дат'
+                normaldate = datetime.datetime.strptime(f"{i.dl}.{today.year}", "%d.%m.%Y").date()
+                t = (normaldate - today).days #це 'Дельта дат'
                 if t <= 0:
                     color = colorama.Fore.RED # це кольори
                 elif t <= 3:
@@ -47,7 +50,7 @@ class TaskMGR: #це основа робота планувальника
                     color = colorama.Fore.GREEN
             except:
                 color = ""
-            print(f"{color}{index}: {i.name} | {i.dedline} | {i.deskr}{colorama.Style.RESET_ALL}") #це сам вивід з форматуванням
+            print(f"{color}{index}: {i.name} | {i.dl} | {i.dsk}{colorama.Style.RESET_ALL}") #це сам вивід з форматуванням
             
 def save_prin(): #це вдобна команда щоби не повторюватись по 100 раз
     MGR.save_file()
@@ -62,8 +65,17 @@ a = input('1-додати 2-прибрать ')
 
 if a == "1":
     b = input("Введи назву ") 
-    c = input("Введи дедлайн ") 
+    c = input("Введи дедлайн ")
     d = input("Введи опис ")
+    
+    if c.startswith('+'):#швидка дата просто +1 замість 23.03
+        try:
+            days = int(c.removeprefix('+'))
+            c = datetime.date.today() + datetime.timedelta(days=days)    
+            c = c.strftime("%d.%m")
+        except:
+            input("Щось не так з +датою ")
+            
     e = Task(b,c,d)
     MGR.add(e)
     save_prin()
@@ -73,9 +85,13 @@ elif a == "2":
     MGR.remov(f)
     save_prin()
 
-elif a not in ("1", "2"): #це швидкий набір по типу Name+1description
-    mat = re.match(r"([^\+\d]+)\+(\d+)(.*)", a) #це розподіл
-    b = mat.group(1).strip() #назва
+elif not a.isdigit(): #це швидкий набір по типу Name+1description
+    mat = re.match(r"([^\+\d]+)\+(\d+)(.*)", a)
+    if not mat:
+        print("Шось не то")
+        input()
+    else:
+        b = mat.group(1).strip()
     try:
         h = int(mat.group(2)) #дедлайн
     except:
